7c565ca155bdf455db27710c88dfad2d1ca33355
mikias-tulu <miki.tulu@gmail.com>
2024-06-26 10:00:00 +0300

    setup app module with moongodb and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './users/user.module';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
